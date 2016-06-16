26fe0d46be584703d916248c1c82bddccb6bc95b
Gabriel Almeida <gabrielgar.almeida@gmail.com>
2016-06-15 21:20:03 -0300

    Analisador de texto + wrapper para lista de discursos

---
+++ AnalisadorTexto.py
@@ -0,0 +1,35 @@
+import nltk
+import unicodedata
+import re
+
+
+NLTK_STOPWORDS_PORTUGUES = nltk.corpus.stopwords.words('portuguese')
+NLTK_STEMMER = nltk.stem.RSLPStemmer().stem
+
+
+class Analisador:
+    def __init__(self, separa_diacriticos=True, regexp_separador_tokens='[ ]+',
+                 regexp_caracteres_validos='[^a-zA-Z ]', stop=NLTK_STOPWORDS_PORTUGUES,
+                 stemmer = NLTK_STEMMER, tamanho_minimo_token=2):
+        self.stemmer = stemmer
+        self.stop = stop
+        self.separa_diacriticos = separa_diacriticos
+        self.tamanho_minimo_token = tamanho_minimo_token
+
+        self.caracteres_validos = re.compile(regexp_caracteres_validos)
+        self.separador_tokens= re.compile(regexp_separador_tokens)
+
+        self.stop = {self.preprocessador(self.stemmer(palavra)) for palavra in stop}
+
+    def preprocessador(self, txt):
+        txt = txt.lower()
+        if self.separa_diacriticos:
+            txt = unicodedata.normalize('NFD', txt)
+        txt = self.caracteres_validos.sub("", txt)
+        return txt
+
+    def analizador(self, txt):
+        txt = self.preprocessador(txt)
+        tokens = self.separador_tokens.split(txt)
+        return [ self.stemmer(token) for token in tokens
+                 if len(token) >= self.tamanho_minimo_token and token not in self.stop]
--- CamaraWS.py
+++ CamaraWS.py
@@ -1,10 +1,13 @@
 import urllib.request
 import xml.dom.minidom
 import urllib.error
-__author__ = 'gabriel'
+import xmltodict
+
 
 URL_RAIZ = "http://www.camara.gov.br/SitCamaraWS/SessoesReunioes.asmx"
 METODO_TEOR_DISCURSO = "/obterInteiroTeorDiscursosPlenario?codSessao=%s&numOrador=%s&numQuarto=%s&numInsercao=%s"
+METODO_LISTAR_DISCURSOS = "/ListarDiscursosPlenario?dataIni=%s&dataFim=%s&codigoSessao=%s" \
+                          "&parteNomeParlamentar=%s&siglaPartido=%s&siglaUF=%s"
 
 ELEMENTOS_SESSAO_PLENARIO = ["nome", "partido", "uf", "horaInicioDiscurso", "discursoRTFBase64"]
 
@@ -29,6 +32,24 @@ def obterInteiroTeorDiscursosPlenario(cod_sessao, num_orador, num_quarto, num_in
 
     return sessao_dict
 
+def listarDiscursosPlenario(dataIni, dataFim, codigoSessao="", parteNomeParlamentar="",
+                            siglaPartido="", siglaUF=""):
+    url = URL_RAIZ + METODO_LISTAR_DISCURSOS % (dataIni, dataFim, codigoSessao,
+                                                parteNomeParlamentar, siglaPartido, siglaUF)
+    discursos = dict()
+    try:
+        with urllib.request.urlopen(url) as response:
+            xml = response.read()
+            discursos = xmltodict.parse(xml)
+
+    except urllib.error.HTTPError as http_error:
+        if http_error.code == 403:
+            raise Exception("Bloqueado")
+        raise Exception({"code": http_error.code, "reason": http_error.reason})
+
+    return discursos
+
 
 if __name__ == "__main__":
-    print(obterInteiroTeorDiscursosPlenario('091.2.55.O', '1','3', '289'))
+    import json
+    print(json.dumps(listarDiscursosPlenario(dataIni='01/06/2016', dataFim='02/06/2016'), indent=4))
