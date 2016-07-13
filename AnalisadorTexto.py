a7868955ba3a91d111f5037e740e279839d886f6
Gabriel Almeida <gabrielgar.almeida@gmail.com>
2016-07-12 22:30:18 -0300

    I give up, for now

--- AnalisadorTexto.py
+++ AnalisadorTexto.py
@@ -1,6 +1,9 @@
 import nltk
 import unicodedata
 import re
+import json
+import collections
+import LimpezaTextoCamara
 
 
 NLTK_STOPWORDS_PORTUGUES = nltk.corpus.stopwords.words('portuguese')
@@ -9,8 +12,8 @@ NLTK_STEMMER = nltk.stem.RSLPStemmer().stem
 
 class Analisador:
     def __init__(self, separa_diacriticos=True, regexp_separador_tokens='[ ]+',
-                 regexp_caracteres_validos='[^a-zA-Z ]', stop=NLTK_STOPWORDS_PORTUGUES,
-                 stemmer = NLTK_STEMMER, tamanho_minimo_token=2):
+                 regexp_caracteres_validos='[^a-zA-Z ]', stop=set(),
+                 stemmer=NLTK_STEMMER, tamanho_minimo_token=2):
 
         self.stemmer = stemmer
         self.stop = stop
@@ -18,10 +21,10 @@ class Analisador:
         self.tamanho_minimo_token = tamanho_minimo_token
 
         self.caracteres_validos = re.compile(regexp_caracteres_validos)
-        self.separador_tokens= re.compile(regexp_separador_tokens)
+        self.separador_tokens = re.compile(regexp_separador_tokens)
 
-        if stemmer is not None:
-            self.stop = {self.preprocessador(self.stemmer(palavra)) for palavra in stop}
+        # if stemmer is not None:
+        #     self.stop = {self.preprocessador(self.stemmer(palavra)) for palavra in stop}
 
     def preprocessador(self, txt):
         txt = txt.lower()
@@ -31,12 +34,102 @@ class Analisador:
         return txt
 
     def analizador(self, txt):
+        """
+            Retorna uma lista de pares contendo o token pré-processado
+            original e o token "stemizado" para futuras análises, caso exista stemming.
+        """
         txt = self.preprocessador(txt)
         tokens = self.separador_tokens.split(txt)
 
         if self.stemmer is not None:
-            return [self.stemmer(token) for token in tokens
-                     if len(token) >= self.tamanho_minimo_token and token not in self.stop]
+            return [(self.stemmer(token), token) for token in tokens
+                    if len(token) >= self.tamanho_minimo_token and token not in self.stop]
         else:
             return [token for token in tokens
-                 if len(token) >= self.tamanho_minimo_token and token not in self.stop]
+                    if len(token) >= self.tamanho_minimo_token and token not in self.stop]
+
+
+class PreprocessadorDiscursos:
+    def __init__(self, analisador=Analisador()):
+        self.contador_palavras_por_token = dict()
+        self.contador_tokens_por_politico = dict()
+
+        self.discursos_por_politicos = dict()
+
+        self.tokens = list()
+        self.analisador = analisador
+
+    def _contabiliza_token(self, token, palavra_original, politico):
+        if token not in self.contador_palavras_por_token:
+            self.contador_palavras_por_token = collections.Counter()
+        self.contador_palavras_por_token[token].update(palavra_original)
+
+        if politico not in self.contador_tokens_por_politico:
+            self.contador_tokens_por_politico = collections.Counter()
+        self.contador_tokens_por_politico[politico].update(token)
+
+    def carrega_documentos(self, arq_jsons_discursos):
+        id_discurso = 0
+        id_politico = 0
+        self.discursos_por_politicos = dict()
+        with open(arq_jsons_discursos) as jsons:
+            for linha in jsons:
+                # Simples analise do progresso
+                if id_discurso % 100 == 0:
+                    print(id_discurso)
+
+                discurso_dict = json.loads(linha)
+
+                texto_discurso = discurso_dict.get('sumarioDiscurso')
+                partido_orador = discurso_dict.get('partidoOrador')
+                uf_orador = discurso_dict.get('ufOrador')
+                nome_orador = discurso_dict.get('nomeOrador')
+
+                if not all([texto_discurso, partido_orador, uf_orador, nome_orador]):
+                    continue
+
+                texto_limpo = texto_discurso
+
+                # IMPORTANTE: Descomentar quando for usar o discurso completo de fato
+                # texto_limpo = LimpezaTextoCamara.limpa_texto(texto_discurso, nome_orador)
+                # if texto_limpo is None:
+                #     continue
+
+                nome_orador = LimpezaTextoCamara.trata_nome(nome_orador)
+                chave = nome_orador + '_' + partido_orador + '_' + uf_orador
+
+                # Estrutura auxiliar para armazenar os discursos
+                resumo_discurso = {'idDiscurso': id_discurso,
+                                   'descricaoFaseSessao': discurso_dict.get('descricaoFaseSessao'),
+                                   'tipoSessao': discurso_dict.get('tipoSessao'),
+                                   'sumarioDiscurso': discurso_dict.get('sumarioDiscurso'),
+                                   'horaInicioDiscurso': discurso_dict.get('horaInicioDiscurso'),
+                                   'texto': texto_limpo}
+
+                # Mantenho uma relacao dos discursos por politico: dicionario "politico" -> "lista de discursos"
+                if chave not in self.discursos_por_politicos:
+                    self.discursos_por_politicos[chave] = dict()
+                    self.discursos_por_politicos[chave]['discursos'] = list()
+                self.discursos_por_politicos[chave]['discursos'].append(resumo_discurso)
+
+                # Armazeno um id para cada politico neste dicionario para futuras referencias
+                if 'idPolitico' not in self.discursos_por_politicos[chave]:
+                    self.discursos_por_politicos[chave]['idPolitico'] = id_politico
+                    id_politico += 1
+
+                # Processo e conto os tokens
+                tokens_discurso = self.analisador.analizador(texto_limpo)
+                for token_processado, token_original in tokens_discurso:
+                    self._contabiliza_token(token_processado, token_original, chave)
+
+                self.tokens.append(tokens_discurso)
+
+                # Preparo proxima iteracao
+                id_discurso += 1
+
+    def salva_documentos(self, ):
+        pass
+
+
+if __name__ == "__main__":
+    pass
--- ModeloVetorial.py
+++ ModeloVetorial.py
@@ -1,7 +1,6 @@
 import sklearn
 import sklearn.externals
 import json
-import LimpezaTextoCamara
 import AnalisadorTexto
 
 
@@ -9,17 +8,15 @@ ANALISADOR_PADRAO = AnalisadorTexto.Analisador()
 
 
 class ModeloVetorial:
-    def __init__(self, analyzer=ANALISADOR_PADRAO.analizador, norma='l2', max_features=None, max_df=1.0, min_df=0.0,
+    def __init__(self, norma='l2', max_features=None, max_df=1.0, min_df=0.0,
                  binary=False, use_idf=True, smooth_idf=True, sublinear_tf=False):
         """ Construtor apenas realiza um wrapping do construtor do sklearn """
         self.vetorizador = sklearn.feature_extraction.text.TfidfVectorizer(
-            analyzer=analyzer, max_features=max_features, norm=norma, max_df=max_df,
+            max_features=max_features, norm=norma, max_df=max_df,
             min_df=min_df, binary=binary, use_idf=use_idf, smooth_idf=smooth_idf,
             sublinear_tf=sublinear_tf)
-        self.discursos_por_politicos = dict()
 
     def gera_modelo_vetorial(self, arq_jsons_discursos, arq_modelo, arq_vetores, arq_resumo_json):
-        iterador = self.__carrega_documentos(arq_jsons_discursos)
         vetores = self.vetorizador.fit_transform(iterador)
 
         sklearn.externals.joblib.dump(self.vetorizador, arq_modelo)
@@ -33,73 +30,13 @@ class ModeloVetorial:
             dump = json.dumps(self.discursos_por_politicos, sort_keys=True)
             arq.write(dump)
 
-    # def iterador_politico(self):
-    #     politicos = list(self.discursos_por_politicos.keys())
-    #     politicos.sort(key=lambda x: self.discursos_por_politicos[x]['idPolitico'])
-    #     for politico in politicos:
-    #         texto_completo = " ".join([discurso['texto']
-    #                                    for discurso in self.discursos_por_politicos[politico]['discursos']])
-    #         yield texto_completo
-
-    def __carrega_documentos(self, arq_jsons_discursos):
-        # TODO Refatorar
-
-        id_discurso = 0
-        id_politico = 0
-        self.discursos_por_politicos = dict()
-        with open(arq_jsons_discursos) as jsons:
-            for linha in jsons:
-                # progresso
-                if id_discurso % 100 == 0:
-                    print(id_discurso)
-
-                discurso_dict = json.loads(linha)
-
-                texto_discurso = discurso_dict.get('sumarioDiscurso')
-                partido_orador = discurso_dict.get('partidoOrador')
-                uf_orador = discurso_dict.get('ufOrador')
-                nome_orador = discurso_dict.get('nomeOrador')
-
-                if not all([texto_discurso, partido_orador, uf_orador, nome_orador]):
-                    continue
-
-                texto_limpo = texto_discurso
-                # texto_limpo = LimpezaTextoCamara.limpa_texto(texto_discurso, nome_orador)
-                # if texto_limpo is None:
-                #     continue
-
-                # estrutura auxiliar 1
-                nome_orador = LimpezaTextoCamara.trata_nome(nome_orador)
-                chave = nome_orador + '_' + partido_orador + '_' + uf_orador
-
-                resumo_discurso = {'idDiscurso': id_discurso,
-                                   'descricaoFaseSessao': discurso_dict.get('descricaoFaseSessao'),
-                                   'tipoSessao': discurso_dict.get('tipoSessao'),
-                                   'sumarioDiscurso': discurso_dict.get('sumarioDiscurso'),
-                                   'horaInicioDiscurso': discurso_dict.get('horaInicioDiscurso'),
-                                   'texto': texto_limpo}
-
-                if chave not in self.discursos_por_politicos:
-                    self.discursos_por_politicos[chave] = dict()
-                    self.discursos_por_politicos[chave]['discursos'] = list()
-                self.discursos_por_politicos[chave]['discursos'].append(resumo_discurso)
-
-                if 'idPolitico' not in self.discursos_por_politicos[chave]:
-                    self.discursos_por_politicos[chave]['idPolitico'] = id_politico
-                    id_politico += 1
-
-                yield texto_limpo
-                id_discurso += 1
-
-
 if __name__ == "__main__":
-    prefixo = "saida_resumo_tfidf_sem_norma_stemmer/"
+    prefixo = "saida_resumo_tfidf_sem_norma_1024/"
     arq_modelo = prefixo + "modelo"
     arq_vetores = prefixo + "matriz"
     arq_resumo_json = prefixo + "resumo_discursos.json"
 
-    modelo = ModeloVetorial(use_idf=True, norma=None, max_df=0.99, max_features=256,
-                            analyzer=AnalisadorTexto.Analisador().analizador)
+    modelo = ModeloVetorial(use_idf=True, norma=None, max_df=0.99, max_features=1024)
 
     modelo.gera_modelo_vetorial("coleta_15-06-2016_23_48_09.json", arq_modelo, arq_vetores, arq_resumo_json)
 
